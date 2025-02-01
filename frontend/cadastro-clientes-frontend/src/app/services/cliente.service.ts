440789b5f92792015108c352d1d4758731fffe19
carolinasdias <carolinasdias06@gmail.com>
2025-02-01 14:10:05 -0300

    criando service

---
+++ frontend/cadastro-clientes-frontend/src/app/services/cliente.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Cliente } from '../models/cliente';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClienteService {
+  private apiUrl = 'http://localhost:5000/api/clientes'; // Altere para a URL do seu backend
+
+  constructor(private http: HttpClient) { }
+
+  getClientes(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.apiUrl);
+  }
+
+  getCliente(id: number): Observable<Cliente> {
+    return this.http.get<Cliente>(`${this.apiUrl}/${id}`);
+  }
+
+  createCliente(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.apiUrl, cliente);
+  }
+
+  updateCliente(id: number, cliente: Cliente): Observable<Cliente> {
+    return this.http.put<Cliente>(`${this.apiUrl}/${id}`, cliente);
+  }
+
+  deleteCliente(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
