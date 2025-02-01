15bb7ac0d09a1df0e0152cdc3a9f113190528006
carolinasdias <carolinasdias06@gmail.com>
2025-02-01 14:09:22 -0300

    Criar componentes de menu, header e footer

--- frontend/cadastro-clientes-frontend/src/app/app.module.ts
+++ frontend/cadastro-clientes-frontend/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuComponent } from './components/menu/menu.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
